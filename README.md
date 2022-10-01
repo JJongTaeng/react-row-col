d5cefc194d070497afb6860d4b6230ac1bc70ae4
조인태 <jjojjo141995@gmail.com>
2022-10-01 22:10:54 +0900

    Update README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+# Download
+```plain
+npm i react-row-col
+```
+
+# Usage
+1. Row
+   - The Row component wraps the Col component and has gutter props.
+   - The gutter props pass an array, and [10, 20] sets the padding of the Col component to top, bottom 10px, left, right 20px.
+
+2. Col
+
+   | props |      width      | description | type |
+   |:---------------:|:-----:|:-----------:|:----:|
+   | `span`  |    all width    | Raster number of cells to occupy, 0 | number |
+   | `xs`    | screen < 576px  | could be a span value | number |
+   | `xs`    | screen < 576px  | could be a span value | number |
+   | `sm`    | screen ≥ 576px  | could be a span value | number |
+   | `md`    | screen ≥ 768px  | could be a span value | number |
+   | `lg`    | screen ≥ 992px  | could be a span value | number |
+   | `xl`    | screen ≥ 1200px | could be a span value | number |
+   | `xxl`   | screen ≥ 1600px | could be a span value | number |
+
+   - `span` props are ignored if props from `xs` to `xxl` are present.
+
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 function App() {
   return (
     <div className="App">
-      <Row>
+      <Row gutter={[10, 10]}>
         <Col span={8}>
           <BorderBox backgroundColor={'red'}>
             Hello
@@ -13,22 +13,70 @@ function App() {
         </Col>
         <Col span={8}>
           <BorderBox backgroundColor={'blue'}>
-            Hello2
+            Hello
           </BorderBox>
         </Col>
         <Col span={8}>
           <BorderBox backgroundColor={'green'}>
-            Hello3
+            Hello
           </BorderBox>
+        </Col>
+        <Col {...{xxl: 24, xl: 8, lg: 8, md: 24, sm: 12, xs: 24}}>
+          <BorderBox backgroundColor={'red'}>
+            Hello
+          </BorderBox>
+        </Col>
+        <Col {...{xxl: 24, xl: 8, lg: 8, md: 24, sm: 12, xs: 24}}>
+          <BorderBox backgroundColor={'blue'}>
+            Hello
+          </BorderBox>
+        </Col>
+        <Col {...{xxl: 24, xl: 8, lg: 8, md: 24, sm: 12, xs: 24}}>
+          <BorderBox backgroundColor={'green'}>
+            Hello
+          </BorderBox>
+        </Col>
+        <Col span={24}>
+          <Row gutter={[20, 20]}>
+            <Col span={12}>
+              <Row>
+                <Col {...{xxl: 24, xl: 24, lg: 12, md: 12, sm: 12, xs: 24}}>
+                  <BorderBox backgroundColor={'blue'}>
+                    Hello
+                  </BorderBox>
+                </Col>
+                <Col {...{xxl: 24, xl: 24, lg: 12, md: 12, sm: 12, xs: 24}}>
+                  <BorderBox backgroundColor={'red'}>
+                    Hello
+                  </BorderBox>
+                </Col>
+                <Col {...{xxl: 24, xl: 24, lg: 12, md: 12, sm: 12, xs: 24}}>
+                  <BorderBox backgroundColor={'blue'}>
+                    Hello
+                  </BorderBox>
+                </Col>
+                <Col {...{xxl: 24, xl: 24, lg: 12, md: 12, sm: 12, xs: 24}}>
+                  <BorderBox backgroundColor={'red'}>
+                    Hello
+                  </BorderBox>
+                </Col>
+              </Row>
+            </Col>
+            <Col span={12}>
+              <BorderBox backgroundColor={'green'} height={400}>
+                Hello
+              </BorderBox>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
   );
 }
 
-const BorderBox = styled.div<{backgroundColor: string}>`
+const BorderBox = styled.div<{backgroundColor: string, height?: number}>`
   border: 1px solid #333; 
-  height: 200px;
+  height: ${({ height }) => height ? height : 200}px;
   display: flex;
   justify-content: center;
   align-items: center;
